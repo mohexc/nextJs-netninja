843e728ede5c38600b65fae633cd19332f2d832c
nanut <galunyu@gmail.com>
2021-02-28 13:06:17 +0700

    [Added] getStaticProps

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,11 +1,30 @@
 import Meta from "../../components/Meta"
+import styles from "../../styles/Ninjas.module.css"
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+  console.log(ninjas)
+  debugger
   return (
     <div>
       <Meta title={'NetNinja | Ninja All'} content={"nextJs"} />
 
       <h1>All Ninjas</h1>
+
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   )
 }
