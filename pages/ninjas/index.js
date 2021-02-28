2e2b4c328c85ec5cedcac0200fd5d3178febc66d
nanut <galunyu@gmail.com>
2021-02-28 14:49:04 +0700

    [Added] getStaticPaths

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import Meta from "../../components/Meta"
 import styles from "../../styles/Ninjas.module.css"
+import Link from "next/link"
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users')
@@ -10,8 +11,6 @@ export const getStaticProps = async () => {
 }
 
 const Ninjas = ({ ninjas }) => {
-  console.log(ninjas)
-  debugger
   return (
     <div>
       <Meta title={'NetNinja | Ninja All'} content={"nextJs"} />
@@ -19,11 +18,11 @@ const Ninjas = ({ ninjas }) => {
       <h1>All Ninjas</h1>
 
       {ninjas.map(ninja => (
-        <div key={ninja.id}>
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
           <a className={styles.single}>
             <h3>{ninja.name}</h3>
           </a>
-        </div>
+        </Link>
       ))}
     </div>
   )
